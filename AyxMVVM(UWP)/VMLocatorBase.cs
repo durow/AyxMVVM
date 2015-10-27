e762f43e4535a4933d988b4b660e97b93212fa83
durow <durow@163.com>
2015-10-27 17:41:10 +0800

    Add VMLocatorBase class

---
+++ AyxMVVM(UWP)/VMLocatorBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace AyxMVVM
+{
+    public abstract class VMLocatorBase
+    {
+        public bool IsDesignMode
+        {
+            get
+            {
+                return DesignMode.DesignModeEnabled;
+            }
+        }
+
+        public TViewModel GetViewModel<TViewModel>() where TViewModel : ViewModelBase
+        {
+            var instance = Activator.CreateInstance<TViewModel>();
+            if (IsDesignMode)
+                instance.InitTestData();
+            else
+                instance.InitRealData();
+            return instance;
+        }
+    }
+}
